3f2d2a9b110aee69574793f99e215005450167a1
Cesar Gonzalez <cesar.g@roserocket.com>
2024-01-20 12:14:43 -0500

    updated

--- models/combined_data.sql
+++ models/combined_data.sql
@@ -1,16 +1,19 @@
 {% call statement('tables_for_union', fetch_result=True) %}
-    SELECT table_name FROM information_schema.tables WHERE table_schema='rrdata'
+    SELECT table_name FROM information_schema.tables WHERE table_schema='rrdata' and table_name != 'combined_data'
 {% endcall %}
 
 {% set tables = load_result('tables_for_union')['data'] %}
 
+{{ config(materialized='incremental', dist='id') }}
+
 with source_data as (
     {% for table in tables %}
         SELECT 
-            to_json(rrdata.trucktrips.*) as jsonData,
+            '{{ table[0] }}' as origin_table,
+            to_json({{ source('postgres', table[0]) }}.*) as jsonData,
             CURRENT_TIMESTAMP as last_updated,
             0 as processed
-        FROM {{ ref(table[0]) }}
+        FROM {{ source('postgres', table[0]) }}
         {% if not loop.last %}
             UNION ALL
         {% endif %}
