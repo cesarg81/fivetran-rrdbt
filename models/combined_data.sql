83fa04c9187b9bd702af40b78f941ceb711f9dae
Cesar Gonzalez <cesar.g@roserocket.com>
2024-01-22 15:47:40 -0500

    updated

--- models/combined_data.sql
+++ models/combined_data.sql
@@ -4,17 +4,28 @@
 
 {% set tables = load_result('tables_for_union')['data'] %}
 
-{{ config(materialized='incremental', dist='id') }}
+{{
+    config(
+        materialized='incremental',
+        unique_key='row_hash'
+    )
+}}
 
 with source_data as (
     {% for table in tables %}
         SELECT 
             '{{ table[0] }}' as origin_table,
-            to_json({{ source('postgres', table[0]) }}.*) as jsonData,
+            to_json(tmp.*) as json_data,
+            md5(CAST(to_json(tmp.*) AS TEXT)) AS row_hash,
             CURRENT_TIMESTAMP as last_updated,
             0 as processed
-        FROM {{ source('postgres', table[0]) }} 
-        WHERE "_fivetran_synced" >= CURRENT_TIMESTAMP - INTERVAL '15 minutes' /* INTERVAL '1 hour'*/
+        FROM ( 
+            SELECT 
+                {{ dbt_utils.star(from=source('postgres', table[0]), except=["_fivetran_synced"]) }}
+            FROM 
+                {{ source('postgres', table[0]) }} 
+            WHERE "_fivetran_synced" >= CURRENT_TIMESTAMP - INTERVAL '15 minutes' 
+        ) tmp
         {% if not loop.last %}
             UNION ALL
         {% endif %}
